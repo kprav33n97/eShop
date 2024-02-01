60f79f07c5d7431403c37d4eb32934b2970b8a57
kprav33n97 <kpraveenkumar2006@gmail.com>
2024-02-01 10:30:27 +0530

    Redux setup

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,11 +6,26 @@ import Filter from '../../components/filter/Filter'
 import ProductCard from '../../components/productCard/ProductCard'
 import Track from '../../components/track/Track'
 import Testimonial from '../../components/testimonial/Testimonial'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart } from '../../redux/cartSlice'
 
 
 function Home() {
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state) => state.cart);
+  console.log(cartItem);
+  const addCart = () => {
+    dispatch(addToCart('shirt'));
+  }
+  const deleteCart = () => {
+    dispatch(deleteFromCart('shirt'));
+  }
   return (
     <Layout>
+      <div className="flex gap-5 justify-center">
+        <button className='bg-gray-300' onClick={() => addCart}>add</button>
+        <button className='bg-gray-300' onClick={() => deleteCart}>del</button>
+      </div>
       <HeroSection/>
       <Filter/>
       <ProductCard/>
